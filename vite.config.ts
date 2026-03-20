23cc79393fe828eb6c5f9b946dc4b1e0d6d513ed
namlvai <nam0981028794@gmail.com>
2026-03-20 12:34:58 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base:'/JimengAIv5/',
       server: {
         port: 3000,
         host: '0.0.0.0',
